f147c14d85add7b3f86e1de8a80b5f1de452c3cf
igorkru1337 <krupenni@list.ru>
2021-09-23 03:18:35 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,107 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <ctime>
 
 using namespace std;
 
 class crypt {
 public:
 
-    static string translateToBinaryWithoutRevers(long long a) {
+    static unsigned long long fastPowWithModule(unsigned long long a, unsigned long long x, unsigned long long p) {
+        string xs = translateToBinaryWithoutRevers(x);
+        unsigned long long y = 1, s = a;
+        for (char i : xs) {
+            if ((unsigned long long) (i - 48)) {
+                y = y * s % p;
+            }
+            s = s * s % p;
+        }
+        return y;
+    }
+
+    static unsigned long long euclideanAlgorithm(unsigned long long a, unsigned long long b) {
+        unsigned long long r;
+        while (b != 0) {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+
+    static void generalizedEuclideanAlgorithm(unsigned long long a, unsigned long long b, unsigned long long *s) {
+        unsigned long long u[3] = {a, 1, 0}, v[3] = {b, 0, 1}, T[3], q;
+        while (v[0] != 0) {
+            q = u[0] / v[0];
+            T[0] = u[0] % v[0];
+            T[1] = u[1] - q * v[1];
+            T[2] = u[2] - q * v[2];
+
+            for (unsigned long long i = 0; i < 3; i++) {
+                u[i] = v[i];
+                v[i] = T[i];
+                s[i] = u[i];
+            }
+        }
+    }
+
+    static unsigned long long diffieHellmanAlgorithm(unsigned long long secretKeyA, unsigned long long secretKeyB) {
+        unsigned long long bottomRange = 100000000, topRange = 500000000;
+
+        unsigned long long q = randBigPrime(bottomRange, topRange);
+        unsigned long long p = 2 * q + 1;
+
+        unsigned long long g;
+        while (true) {
+            std::random_device rd;
+            std::mt19937 mt(rd());
+            mt.seed(time(nullptr));
+            std::uniform_int_distribution<unsigned long long> dist;
+            g = dist(mt) % (p - 1);
+            if (g < p && fastPowWithModule(g, q, p) != 1)
+                break;
+        }
+        cout << "q = " << q << endl << "p = " << p << endl << "g = " << g << endl;
+
+        unsigned long long openKeyA = fastPowWithModule(g, secretKeyA, p);
+        unsigned long long openKeyB = fastPowWithModule(g, secretKeyB, p);
+
+        unsigned long long linkNumberAB = fastPowWithModule(openKeyB, secretKeyA, p);
+        unsigned long long linkNumberBA = fastPowWithModule(openKeyA, secretKeyB, p);
+
+        if (linkNumberAB == linkNumberBA) {
+            cout << linkNumberAB << " = " << linkNumberBA;
+            cout << endl << "connected" << endl << endl;
+        } else {
+            cout << endl << "connection failed" << endl << endl;
+        }
+    }
+
+    static unsigned long long babyStepGiantStep(unsigned long long a, unsigned long long y) {
+        long long int bottomRange = 100000000, topRange = 500000000;
+        long long int p = 7;
+        long long int m = (int) sqrt(p) + 1;
+        long long int k = (int) sqrt(p) + 1;
+        unsigned long long s[m], i, j;
+
+        for (i = 0; i < m; i++) s[i] = fastPowWithModule(y * pow(a, i), 1, p);
+        for (i = 0; i < k; i++) {
+            for (j = 0; j < m; j++) {
+                if (s[j] == fastPowWithModule(pow(a, (i + 1) * m), 1, p)) {
+                    break;
+                }
+            }
+            if (j != m) {
+                break;
+            }
+        }
+        cout << "p = " << p << endl;
+        cout << "check = " << fastPowWithModule(2,((i + 1) * m - j),p) << endl;
+        return ((i + 1) * m - j);
+    }
+
+    static string translateToBinaryWithoutRevers(unsigned long long a) {
         string res;
         while (a != 0) {
             res += (char) (a % 2 + 48);
@@ -15,85 +111,43 @@ public:
         return res;
     }
 
-    static long long fastPowWithModule(long long a, long long b, long long p) {
-        string xs = translateToBinaryWithoutRevers(b);
-        long long y = 1, s = a;
-        for (char i : xs) {
-            if ((long long) (i - 48)) {
-                y = y * s % p;
-            }
-            s = s * s % p;
-        }
-        return y;
-    }
-
-    static long long euclideanAlgorithm(long long a, long long b) {
-        long long r;
-        while (b != 0) {
-            r = a % b;
-            a = b;
-            b = r;
-        }
-        return a;
-    }
-
-    static void generalizedEuclideanAlgorithm(long long a, long long b, long long *s) {
-        long long u[3] = {a, 1, 0}, v[3] = {b, 0, 1}, T[3], q;
-        while (v[0] != 0) {
-            q = u[0] / v[0];
-            T[0] = u[0] % v[0];
-            T[1] = u[1] - q * v[1];
-            T[2] = u[2] - q * v[2];
-
-            for (long long i = 0; i < 3; i++) {
-                u[i] = v[i];
-                v[i] = T[i];
-                s[i] = u[i];
-            }
+    static unsigned long long randBigPrime(unsigned long long lower, unsigned long long upper) {
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        mt.seed(time(nullptr));
+        std::uniform_int_distribution<unsigned long long> dist(lower, upper);
+        while (true) {
+            unsigned long long p = 1 | dist(mt);
+            if (isPrime(p)) return p;
         }
     }
 
-    static long long diffieHellmanAlgorithm(long long p, long long a, long long b) {
-        long long q = (p - 1) / 2, g;
-        long long mas[10] = {3, 5, 7, 11, 13, 17, 23, 29, 31, 37};
-        for (long long i : mas)
-            if (fastPowWithModule(i, q, p) == 1) {
-                g = mas[1];
-                break;
-            }
-        long long a1 = fastPowWithModule(g, a, p);
-        long long b1 = fastPowWithModule(g, b, p);
-        cout << fastPowWithModule(b1, a, p) << " = " << fastPowWithModule(a1, b, p) << endl;
-        return g;
-    }
+    static unsigned long long isPrime(unsigned long long n) {
 
-    static long long babyStepGiantStep(long long a, long long p, long long y) {
-        long long m = p / 2 - 1, k = p / 2 + 1, s[m], b[k], i, j;
-        for (i = 0; i < m; i++) s[i] = fastPowWithModule(y * pow(a, i), 1, p);
-        for (i = 0; i < k; i++) b[i] = fastPowWithModule(pow(a, (i + 1) * m), 1, p);
-        for (i = 0; i < k; i++) {
-            for (j = 0; j < m; j++) {
-                if (s[j] == b[i]) break;
-            }
-            if (j != m) break;
+        if ((n & 1) == 0) return n == 2;
+        if (n % 3 == 0) return n == 3;
+        if (n < 25) return n > 1;
+        unsigned long long p = 5;
+        while (p * p <= n) {
+            if (n % p == 0) return 0;
+            p += 2;
+            if (n % p == 0) return 0;
+            p += 4;
         }
-        return ((i + 1) * m - j);
+        return 1;
     }
 };
 
 int main() {
 
-    long long s[3];
+    unsigned long long s[3];
 
     cout << crypt::fastPowWithModule(3, 100, 7) << endl;
 
     //cout<<a.euclideanAlgorithm(28,8);
     crypt::generalizedEuclideanAlgorithm(28, 19, s);
     cout << s[0] << " " << s[1] << " " << s[2] << endl;
-
-    cout << crypt::diffieHellmanAlgorithm(23, 7, 13) << " " << crypt::diffieHellmanAlgorithm(7, 5, 11) << endl;
-
-    cout << crypt::babyStepGiantStep(3, 7, 4);
-
+    cout << crypt::diffieHellmanAlgorithm(2, 7) << " " << crypt::diffieHellmanAlgorithm(7, 5) << endl;
+    cout << crypt::babyStepGiantStep(3, 4);
 
 }
